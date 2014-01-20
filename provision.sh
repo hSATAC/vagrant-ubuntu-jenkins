0d2d2d732847bcea87818602e8916652d2bceecb
Chris Moreton <chris@netsensia.com>
2014-01-20 16:17:25 +0000

    Add plugins

--- provision.sh
+++ provision.sh
@@ -20,3 +20,29 @@ cp /vagrant/VirtualHost/jenkins.conf /etc/apache2/sites-available
 a2ensite jenkins.conf
 service apache2 restart
 
+#############################################################
+# Install Jenkins plugins
+#############################################################
+apt-get -y install curl
+cd /var/lib/jenkins/plugins
+
+curl -OL http://updates.jenkins-ci.org/latest/analysis-core.hpi
+curl -OL http://updates.jenkins-ci.org/latest/ccm.hpi
+curl -OL http://updates.jenkins-ci.org/latest/checkstyle.hpi
+curl -OL http://updates.jenkins-ci.org/latest/cloverphp.hpi
+curl -OL http://updates.jenkins-ci.org/latest/dry.hpi
+curl -OL http://updates.jenkins-ci.org/latest/git.hpi
+curl -OL http://updates.jenkins-ci.org/latest/git-client.hpi
+curl -OL http://updates.jenkins-ci.org/latest/jdepend.hpi
+curl -OL http://updates.jenkins-ci.org/latest/jshint-checkstyle.hpi
+curl -OL http://updates.jenkins-ci.org/latest/plot.hpi
+curl -OL http://updates.jenkins-ci.org/latest/pmd.hpi
+curl -OL http://updates.jenkins-ci.org/latest/postbuild-task.hpi
+curl -OL http://updates.jenkins-ci.org/latest/violations.hpi
+curl -OL http://updates.jenkins-ci.org/latest/xunit.hpi
+
+chown -R jenkins:nogroup *
+
+curl -X POST http://jenkins.local/reload
+/etc/init.d/jenkins restart
+
